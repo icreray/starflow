238af6ec32e631ffb7bb6f0862a0394caa336b24
Creray <creray.default@gmail.com>
2025-05-04 00:17:49 +0700

    Add env_logger

--- client/app/src/main.rs
+++ client/app/src/main.rs
@@ -1,9 +1,8 @@
-use default::default;
 use futures_lite::future;
 
 use glued::ModularApp;
-use starflow_render::RenderModule;
-use starflow_window::{WindowModule, WinitRunner};
+use starflow_render::{GpuContextConfig, RenderModule};
+use starflow_window::{WinitWindow, FromWindow, WindowModule, WinitRunner};
 
 #[derive(ModularApp)]
 struct ClientApp<'window>(
@@ -11,7 +10,25 @@ struct ClientApp<'window>(
 	RenderModule<'window>
 );
 
+// FIXME: Better way to initialize app
+impl<'w> FromWindow for ClientApp<'w> {
+	fn from(window: WinitWindow) -> Self {
+		let window_module = WindowModule::new(window)
+			.with_title("Starflow");
+		let context = window_module.create_context();
+		let reder_module = future::block_on(
+			RenderModule::new(
+				GpuContextConfig::default(), 
+				context.window, 
+				context.size
+			)
+		);
+		Self (window_module, reder_module)
+	}
+}
+
 pub fn main() {
+	env_logger::init();
 	let render_module = future::block_on(
 		RenderModule::new(default())
 	);
