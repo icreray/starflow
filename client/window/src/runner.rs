8c475315301ae0f5cc716e1cd003bdfeea66b3e5
Creray <creray.default@gmail.com>
2025-06-06 12:55:44 +0700

    [Refactor] Collapse runner into single file

--- client/window/src/runner.rs
+++ client/window/src/runner.rs
@@ -1,11 +1,34 @@
 use default::default;
+use log::error;
 
 use winit::{
-	application::ApplicationHandler, event::WindowEvent, 
-	event_loop::ActiveEventLoop, window::{Window, WindowId}
+	application::ApplicationHandler, event::WindowEvent,
+	event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
+	window::{Window, WindowId}
 };
 
-use glued::ModularApp;
+use glued::{AppRunner, ModularApp};
+
+
+pub struct WinitRunner;
+impl AppRunner for WinitRunner {
+	type Context = Window;
+
+	fn run<A>()
+	where A: ModularApp + From<Self::Context> {
+		// FIXME replace `except` with proper error handling
+		let event_loop = EventLoop::new()
+			.expect("Failed to create event loop");
+
+		event_loop.set_control_flow(ControlFlow::Poll);
+
+		let mut handler = AppHandler::<A>::default();
+		if let Err(err) = event_loop.run_app(&mut handler) {
+			error!("winit event_loop exited with an error: {}", err);
+		}
+	}
+}
+
 
 pub(super) struct AppHandler<A>
 where A: ModularApp {
