bf7355592004876793d3c3215462859826a2ffff
Creray <creray.default@gmail.com>
2025-08-28 00:00:45 +0700

    Remove key dublication at BindGroupLayout creation

--- client/render/src/resources/bind_groups.rs
+++ client/render/src/resources/bind_groups.rs
@@ -1,7 +1,7 @@
 use std::ops::Index;
 use ahash::AHashMap;
 
-use wgpu::{BindGroupLayout, BindGroupLayoutDescriptor, Device};
+use wgpu::{BindGroupLayout, BindGroupLayoutDescriptor, BindGroupLayoutEntry, Device};
 
 
 #[derive(Default)]
@@ -15,20 +15,20 @@ pub(crate) struct BindGroupLayoutId(usize);
 
 impl BindGroupLayouts {
 
-	// TODO: 'Neat' layout creation
 	pub fn add(
 		&mut self,
 		device: &Device,
 		key: &'static str,
-		descriptor: &BindGroupLayoutDescriptor
+		entries: &[BindGroupLayoutEntry]
 	) -> Option<BindGroupLayoutId> {
 		if self.key_to_id.contains_key(key) {
 			None
 		}
 		else {
+			let descriptor = BindGroupLayoutDescriptor { label: Some(key), entries };
 			let id = BindGroupLayoutId(self.layouts.len());
 			self.key_to_id.insert(key, id);
-			self.layouts.push(device.create_bind_group_layout(descriptor));
+			self.layouts.push(device.create_bind_group_layout(&descriptor));
 			Some(id)
 		}
 	}
@@ -52,7 +52,7 @@ impl Index<BindGroupLayoutId> for BindGroupLayouts {
 
 
 pub(crate) mod assets {
-	use wgpu::{BindGroupLayoutDescriptor, Device, StorageTextureAccess, TextureFormat};
+	use wgpu::{Device, StorageTextureAccess, TextureFormat};
 
 	use crate::{core::util::bind_group_layout::binding, resources::BindGroupLayouts};
 
@@ -60,22 +60,16 @@ pub(crate) mod assets {
 	// TODO: Move this outside renderer
 	pub(crate) fn create_bind_group_layouts(device: &Device) -> BindGroupLayouts {
 		let mut layouts = BindGroupLayouts::default();
-		layouts.add(device, "output_texture", &BindGroupLayoutDescriptor {
-			label: Some("output_texture"),
-			entries: &[
+		layouts.add(device, "output_texture", &[
 				binding(0)
 					.compute()
 					.texture_storage_2d(TextureFormat::Rgba8Unorm, StorageTextureAccess::WriteOnly)
-			]
-		});
-		layouts.add(device, "input_texture", &BindGroupLayoutDescriptor {
-			label: Some("input_texture"),
-			entries: &[
+		]);
+		layouts.add(device, "input_texture", &[
 				binding(0)
 					.fragment()
 					.texture_storage_2d(TextureFormat::Rgba8Unorm, StorageTextureAccess::ReadOnly)
-			]
-		});
+		]);
 		layouts
 	}
 }
